d9b9fc89cc4d04acc317f5e9a8eb7b0f6e1cb8ae
Yukta Choudhary <142574390+yukta-2k23@users.noreply.github.com>
2025-01-14 23:25:28 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='h-screen bg-[#EBF0FF] flex flex-col items-center justify-center'>
       <h1 className='text-[6rem] font-sans font-extrabold text-[#2A52BE]'>Hello!</h1>
       <h5 className='text-sm font-medium font-sans  text-[#202841] text-center mt-[-1rem]'>One stop Contact app for managing information <br /> about everyone you know!</h5>
-      <img className = "mt-2" src="../src/assets/avatar.png " alt="avatar" />
+      <img className = "mt-2" src="./assets/avatar.png " alt="avatar" />
     </div>
   )
 }
